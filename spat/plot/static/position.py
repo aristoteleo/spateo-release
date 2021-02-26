65da12ed88eb4b34c3fa648dfd1d712051b0da16
Xiaojie Qiu <xqiu@uw.edu>
2021-02-26 18:23:31 -0500

    set up the basic architecture of spat

---
+++ spat/plot/static/position.py
@@ -0,0 +1,7 @@
+from dynamo.plot.scatters import scatters
+
+def position(adata, skey='position', **kwargs):
+    if skey in adata.obsm_keys():
+        x, y = adata.obsm[skey][:, 0], adata.obsm[skey][:, 1]
+
+    scatters(adata, x=x, y=y, **kwargs)
---
+++ spat/preprocessing/__init__.py
@@ -0,0 +1,5 @@
+"""A complete solution of spatialtemporal dynamics analyses toolkit of single cell spatial transcriptomics
+"""
+
+from .image import *
+from .sequencing import *
