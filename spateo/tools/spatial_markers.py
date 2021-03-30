eb6fc5a1e63258405ce1ebb05304dc1770b84703
Xiaojie Qiu <xqiu.sc@gmail.com>
2021-03-29 20:09:22 -0400

    add the local Moran's I test

--- spateo/tools/spatial_markers.py
+++ spateo/tools/spatial_markers.py
@@ -1,3 +1,85 @@
+from tqdm import tqdm
+
+def cluster_specific_genes(adata, 
+                           group,
+                           spatial_key='spatial',
+                           genes=None,
+                           layer=None,
+                           n_neighbors=8,
+                          ): 
+        group_num = adata.obs[group].value_counts()
+
+        group_name = adata.obs[group]
+
+        uniq_g, group_name = group_name.unique(), group_name.to_list()
+
+        # Generate W from the GeoDataFrame
+        w = weights.distance.KNN.from_array(E9_5.obsm['spatial'], k=n_neighbors)
+
+        # Row-standardization
+        w.transform = 'R'
+
+        if genes is None:
+            genes = adata.var.index[adata.var.use_for_pca]
+        else:
+            genes = adata.var.index.intersection(genes)
+
+        db = pandas.DataFrame(adata.obsm['spatial'], columns=['x', 'y'])
+
+        adata.var['hotspot_num'], adata.var['hotspot_frac'], adata.var['hotspot_spec'] = None, None, None,
+        adata.var['coldspot_num'], adata.var['coldspot_frac'], adata.var['coldspot_spec'] = None, None, None,
+        adata.var['doughnut_num'], adata.var['doughnut_frac'], adata.var['doughnut_spec'] = None, None, None,
+        adata.var['diamond_num'], adata.var['diamond_frac'], adata.var['diamond_spec'] = None, None, None,
+
+        adata.var['hotspot_num_val'], adata.var['hotspot_frac_val'], adata.var['hotspot_spec_val'] = None, None, None,
+        adata.var['coldspot_num_val'], adata.var['coldspot_frac_val'], adata.var['coldspot_spec_val'] = None, None, None,
+        adata.var['doughnut_num_val'], adata.var['doughnut_frac_val'], adata.var['doughnut_spec_val'] = None, None, None,
+        adata.var['diamond_num_val'], adata.var['diamond_frac_val'], adata.var['diamond_spec_val'] = None, None, None,
+
+        hotspot_num, hotspot_frac, hotspot_spec = np.zeros(len(uniq_g)), np.zeros(len(uniq_g)), np.zeros(len(uniq_g))
+        coldspot_num, coldspot_frac, coldspot_spec = np.zeros(len(uniq_g)), np.zeros(len(uniq_g)), np.zeros(len(uniq_g))
+        doughnut_num, doughnut_frac, doughnut_spec = np.zeros(len(uniq_g)), np.zeros(len(uniq_g)), np.zeros(len(uniq_g))
+        diamond_num, diamond_frac, diamond_spec = np.zeros(len(uniq_g)), np.zeros(len(uniq_g)), np.zeros(len(uniq_g))
+
+        valid_inds_list = [np.array(group_name) == g for g in uniq_g]
+        
+        for i, cur_g in tqdm(enumerate(genes), desc="performing local Moran I analysis and assign genes and significant domaints to cell type"):
+            if layer is None:
+                db['exp'] = adata[:, cur_g].X.A.flatten()
+            else:
+                db['exp'] = adata[:, cur_g].layers[layer].A.flatten()
+
+            db['w_exp'] = weights.spatial_lag.lag_spatial(w, db['exp'])
+
+            lisa = esda.moran.Moran_Local(db['exp'], w)
+
+            sig = 1 * (lisa.p_sim < 0.05)
+
+            hotspot = 1 * (sig * lisa.q==1)
+            coldspot = 3 * (sig * lisa.q==3)
+            doughnut = 2 * (sig * lisa.q==2)
+            diamond = 4 * (sig * lisa.q==4)
+
+            for ind, g in enumerate(uniq_g):   
+                valid_inds = valid_inds_list[ind]
+                hotspot_num[ind], hotspot_frac[ind], hotspot_spec[ind] = sum(hotspot[valid_inds]), sum(hotspot[valid_inds]) / group_num[g], sum(hotspot[valid_inds]) / sum(hotspot)
+                coldspot_num[ind], coldspot_frac[ind], coldspot_spec[ind] = sum(coldspot[valid_inds]), sum(coldspot[valid_inds]) / group_num[g], sum(coldspot[valid_inds]) / sum(coldspot)
+                doughnut_num[ind], doughnut_frac[ind], doughnut_spec[ind] = sum(doughnut[valid_inds]), sum(doughnut[valid_inds]) / group_num[g], sum(doughnut[valid_inds]) / sum(doughnut)
+                diamond_num[ind], diamond_frac[ind], diamond_spec[ind] = sum(diamond[valid_inds]), sum(diamond[valid_inds]) / group_num[g], sum(diamond[valid_inds]) / sum(diamond)
+
+            adata.var.loc[cur_g, 'hotspot_num_val'], adata.var.loc[cur_g, 'hotspot_frac_val'], adata.var.loc[cur_g, 'hotspot_spec_val'] = np.max(hotspot_num), np.max(hotspot_frac), np.max(hotspot_spec)
+            adata.var.loc[cur_g, 'coldspot_num_val'], adata.var.loc[cur_g, 'coldspot_frac_val'], adata.var.loc[cur_g, 'coldspot_spec_val'] = np.max(coldspot_num), np.max(coldspot_frac), np.max(coldspot_spec)
+            adata.var.loc[cur_g, 'doughnut_num_val'], adata.var.loc[cur_g, 'doughnut_frac_val'], adata.var.loc[cur_g, 'doughnut_spec_val'] = np.max(doughnut_num), np.max(doughnut_frac), np.max(doughnut_spec)
+            adata.var.loc[cur_g, 'diamond_num_val'], adata.var.loc[cur_g, 'diamond_frac_val'], adata.var.loc[cur_g, 'diamond_spec_val'] = np.max(diamond_num), np.max(diamond_frac), np.max(diamond_spec)
+            
+            adata.var.loc[cur_g, 'hotspot_num'], adata.var.loc[cur_g, 'hotspot_frac'], adata.var.loc[cur_g, 'hotspot_spec'] = uniq_g[np.argsort(hotspot_num)[-1]], uniq_g[np.argsort(hotspot_frac)[-1]], uniq_g[np.argsort(hotspot_spec)[-1]]
+            adata.var.loc[cur_g, 'coldspot_num'], adata.var.loc[cur_g, 'coldspot_frac'], adata.var.loc[cur_g, 'coldspot_spec'] = uniq_g[np.argsort(coldspot_num)[-1]], uniq_g[np.argsort(coldspot_frac)[-1]], uniq_g[np.argsort(coldspot_spec)[-1]]
+            adata.var.loc[cur_g, 'doughnut_num'], adata.var.loc[cur_g, 'doughnut_frac'], adata.var.loc[cur_g, 'doughnut_spec'] = uniq_g[np.argsort(doughnut_num)[-1]], uniq_g[np.argsort(doughnut_frac)[-1]], uniq_g[np.argsort(doughnut_spec)[-1]]
+            adata.var.loc[cur_g, 'diamond_num'], adata.var.loc[cur_g, 'diamond_frac'], adata.var.loc[cur_g, 'diamond_spec'] = uniq_g[np.argsort(diamond_num)[-1]], uniq_g[np.argsort(diamond_frac)[-1]], uniq_g[np.argsort(diamond_spec)[-1]]
+
+        return adata 
+      
+      
 def GM_lag_model(adata, 
                 genes,
                 group,
