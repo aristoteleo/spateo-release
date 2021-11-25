70fed1ad3aa093a83a18b4f8b36b825443650464
WangShuai-3 <wangshuai3@genomics.cn>
2021-11-25 10:44:38 +0800

    Update data_io.py
    add function `readBGI_cells`.
    A helper function that facilitates constructing an AnnData object suitable for downstream spateo analysis.
        Using the results of cell segmentation.

--- spateo/data_io.py
+++ spateo/data_io.py
@@ -98,8 +98,11 @@ def readBGI(filename, binsize=50, version='stereo_v1'):
     data["csr_x_ind"] = data["cell_name"].map(cell_dict)
     data["csr_y_ind"] = data["geneID"].map(gene_dict)
 
+    # !Note that in this version, the column name of the gene count value may be 'UMICount' or 'MIDCounts'.
+    count_name = 'UMICount' if 'UMICount' in data.columns else 'MIDCounts'
+
     # Important! by default, duplicate entries are summed together in the following which is needed for us!
-    csr_mat = csr_matrix((data['UMICount'], (data["csr_x_ind"], data["csr_y_ind"])),
+    csr_mat = csr_matrix((data[count_name], (data["csr_x_ind"], data["csr_y_ind"])),
                          shape=((len(uniq_cell), len(uniq_gene))))
 
     var = pd.DataFrame(
@@ -121,4 +124,99 @@ def readBGI(filename, binsize=50, version='stereo_v1'):
     return adata
 
 
+def readBGI_cells(filename,
+                  label_path,
+                  slice: str = None,
+                  version='stereo_v1') -> AnnData:
+    """A helper function that facilitates constructing an AnnData object suitable for downstream spateo analysis.
+    Using the results of cell segmentation.
+
+    Parameters
+    ----------
+        filename: `str`
+            A string that points to the directory and filename of single-cell spatial transcriptomics dataset, produced
+            by the stereo-seq method from BGI. Using the results of cell segmentation.
+        label_path: `str`
+            A string that points to the directory and filename of cell segmentation label matrix(Format:`.npy`).
+            Will be used when displaying with exact shapes of cells.
+        slice: `str`
+            Name of the slice. Will be used when displaying multiple slices.
+        version: `str`
+            The version of technology. Currently not used. But may be useful when the data format changes after we update
+            the stereo-seq technology in future.
+
+    Returns
+    -------
+        adata: :class:`~anndata.AnnData`
+            An AnnData object. Each row of the AnnData object correspond to a cell (results of cell segmentation). The
+            `spatial` key in the .obsm corresponds to the x, y coordinates of the centroids of all cells. The
+            `cell_label` key in the .obs points to the cell segmentation labels of all cells. The `label_mtx` key in the
+            .uns points to the cell segmentation label matrix of this slice.
+    """
+    data = pd.read_csv(filename, header=0, delimiter='\t')
+
+    data['cell_name'] = data['cell'].astype(str)
+
+    uniq_cell, uniq_gene = data.cell_name.unique(), data.geneID.unique()
+    uniq_cell, uniq_gene = list(uniq_cell), list(uniq_gene)
+
+    cell_dict = dict(zip(uniq_cell, range(0, len(uniq_cell))))
+    gene_dict = dict(zip(uniq_gene, range(0, len(uniq_gene))))
+
+    data["csr_x_ind"] = data["cell_name"].map(cell_dict)
+    data["csr_y_ind"] = data["geneID"].map(gene_dict)
+
+    # !Note that in this version, the column name of the gene count value may be 'UMICount' or 'MIDCounts'.
+    count_name = 'UMICount' if 'UMICount' in data.columns else 'MIDCounts'
+
+    # Important! by default, duplicate entries are summed together in the following which is needed for us!
+    csr_mat = csr_matrix((data[count_name], (data["csr_x_ind"], data["csr_y_ind"])),
+                         shape=((len(uniq_cell), len(uniq_gene))))
+
+    label_mtx = np.load(label_path)
+
+    # Calculate the centroid of cells by cell segmentation label matrix
+    coor = centroid_cells(label_mtx)
+
+    var = pd.DataFrame(
+        {"gene_short_name": uniq_gene}
+    )
+    var.set_index("gene_short_name", inplace=True)
+
+    obs = pd.DataFrame(
+        {"cell_name": uniq_cell, "cell_id": [int(i) for i in uniq_cell]}
+    )
+    obs.set_index("cell_name", inplace=True)
+
+    obsm = {
+        "spatial": coor
+    }
+
+    uns = {
+        "label_mtx": {slice: label_mtx}
+    }
+
+    adata = AnnData(csr_mat, obs=obs.copy(), var=var.copy(), obsm=obsm.copy(), uns=uns.copy())
+
+    return adata
+
+
+def centroid_cells(label_mtx):
+    """Calculate the centroid of cells by cell segmentation label matrix.
+
+    Parameters
+    ----------
+        label_mtx: `numpy.ndarray`
+            The cell segmentation label matrix.
+
+    Returns
+    -------
+        coor : `pandas.DataFrame`
+            The x, y coordinates of the centroids of all cells.
+    """
+    coor = pd.DataFrame(label_mtx.flatten(), columns=['cell'])
+    coor = coor[coor['cell'] > 0]
+    coor[['x_centroid', "y_centroid"]] = np.argwhere(label_mtx > 0)
+    coor = coor.groupby('cell').mean().values
+    return coor
 
