7c8e1a4fa143c2f52d6c30ff0ffb6255508bebea
HailinPan <hailinpan1988@163.com>
2021-11-30 15:11:08 +0800

    Update conv.py
    type setting

--- spateo/preprocessing/sequencing/conv.py
+++ spateo/preprocessing/sequencing/conv.py
@@ -10,29 +10,28 @@ def conv(a, ks, circle=False):
 	convolution with specific kernel size of all one kernel
 
 	Parameters
-    ----------
-    a: 2d np array
-    	The source array.
+	----------
+	a: 2d np array
+		The source array.
 
 	ks: an odd int
 		The kernel size of kernel used to perform convolution. The elements of the kernel would be all ones.
 
 	Returns
-    -------
-    c: a 2d np array
-        The returned array has the same type as `a`.
+	-------
+	c: a 2d np array
+	The returned array has the same type as `a`.
 
 
-    Examples
-    --------
-    >>> a = np.array([[1,2,3,4,5,6],[1,1,1,1,1,1],[1,300,1,1,1,1],[1,1,1,1,1,1]])
-    >>> c = conv(a, ks=3, circle=True)
-    >>> c
-    array([[  6,   9,  13,  17,  21,  24],
-       [  5, 305,   7,   8,   9,  10],
-       [304, 304, 304,   5,   5,   5],
-       [  5, 304,   5,   5,   5,   5]])
-
+	Examples
+	--------
+	>>> a = np.array([[1,2,3,4,5,6],[1,1,1,1,1,1],[1,300,1,1,1,1],[1,1,1,1,1,1]])
+	>>> c = conv(a, ks=3, circle=True)
+	>>> c
+	array([[  6,   9,  13,  17,  21,  24],
+	   [  5, 305,   7,   8,   9,  10],
+	   [304, 304, 304,   5,   5,   5],
+	   [  5, 304,   5,   5,   5,   5]])
 	'''
 
 	# check if ks is an odd number
