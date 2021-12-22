af2bc29a47b3508f7fe206bd1a7cd6028d92802d
Lioscro <phoenixter96@gmail.com>
2021-12-22 09:28:18 -0500

    make spateo importable and tests initial commit

--- spateo/plot/static/__init__.py
+++ spateo/plot/static/__init__.py
@@ -1,6 +1,6 @@
 """A complete solution of spatialtemporal dynamics analyses toolkit of single cell spatial transcriptomics
 """
 
-from .three_d_plots import plot_3D
+# from .three_d_plots import plot_3D
 
 from dynamo.plot.space import space
