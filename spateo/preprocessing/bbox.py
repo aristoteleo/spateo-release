375f9ee35a9a6688429d705d35d7772e4d10f58b
Lioscro <phoenixter96@gmail.com>
2021-12-23 15:47:04 -0500

    placeholder test files

---
+++ spateo/preprocessing/bbox.py
@@ -0,0 +1,5 @@
+"""Functions to find bounding box of the tissue on an entire slice using
+total UMIs per pixel. By selecting a bounding box before any preprocessing,
+the hope is that the computational complexity of all downstream computations
+will be reduced.
+"""
