ec87d30464a428b613ead065e65b9513cb5a8bfe
Lioscro <phoenixter96@gmail.com>
2021-12-23 13:05:03 -0500

    add label.py and function placeholders

---
+++ spateo/preprocessing/segmentation/label.py
@@ -0,0 +1,11 @@
+"""Functions for use when labeling individual cells, after obtaining a cell mask.
+"""
+
+
+def label_cells(X: np.ndarray, mask: np.ndarray) -> np.ndarray:
+    """Assign individual cell labels using total UMIs and a cell mask."""
+    pass
+
+
+def enlarge():
+    pass
