6dd752008eb70eba9d6fddcb8cc16aa739c511fd
Jiajun Yao <89448797+Yao-14@users.noreply.github.com>
2022-01-16 15:16:22 +0800

    Update __init__.py

--- spateo/plot/static/__init__.py
+++ spateo/plot/static/__init__.py
@@ -1,6 +1,6 @@
 """A complete solution of spatialtemporal dynamics analyses toolkit of single cell spatial transcriptomics
 """
 
-from .three_d_plots import build_three_d_model, three_d_slicing
+from .three_d_plots import build_three_d_model, three_d_slicing, easy_three_d_plot
 
 from dynamo.plot.space import space
