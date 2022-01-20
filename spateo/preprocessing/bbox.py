c8dc2a1df09244725cd7828f655011939ffdcd6a
Xiaojie Qiu <xqiu@uw.edu>
2022-01-20 17:48:09 -0500

    delete bbox in pp

--- spateo/preprocessing/bbox.py
+++
@@ -1,8 +0,0 @@
-"""Functions to find bounding box of the tissue on an entire slice using
-total UMIs per pixel. By selecting a bounding box before any preprocessing,
-the hope is that the computational complexity of all downstream computations
-will be reduced.
-
-Todo:
-    * Implement @Xiaojieqiu
-"""
