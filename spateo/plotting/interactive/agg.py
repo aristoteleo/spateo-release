310ab8c2cac93ca139a957511ddc68435195cd1e
Lioscro <phoenixter96@gmail.com>
2022-03-09 14:22:38 -0500

    fix contour plotting

--- spateo/plotting/interactive/agg.py
+++ spateo/plotting/interactive/agg.py
@@ -13,13 +13,14 @@ from ...configuration import SKM
 from ...errors import PlottingError
 
 
-def contours(adata: AnnData, layer: str, colors: Optional[List] = None) -> go.Figure:
+def contours(adata: AnnData, layer: str, colors: Optional[List] = None, scale: float = 0.05) -> go.Figure:
     """Interactively display UMI density bins.
 
     Args:
         adata: Anndata containing aggregated UMI counts.
         layer: Layer to display
         colors: List of colors.
+        scale: Scale width and height by this amount.
 
     Returns:
         A Plotly figure
@@ -37,27 +38,28 @@ def contours(adata: AnnData, layer: str, colors: Optional[List] = None) -> go.Fi
             mask = bins == bin
             mtx = mask.astype(np.uint8)
             mtx[mtx > 0] = 255
-            contour = cv2.findContours(mtx, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0][0].squeeze()
-
-            figure.add_trace(
-                go.Scatter(
-                    x=contour[:, 0],
-                    y=-contour[:, 1],
-                    text=str(bin),
-                    line_width=0,
-                    fill="toself",
-                    mode="lines",
-                    showlegend=False,
-                    hoverinfo="text",
-                    hoveron="fills",
-                    fillcolor=mpl.colors.to_hex(colors[color_i % len(colors)]),
+            contours = cv2.findContours(mtx, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+            for contour in contours:
+                contour = contour.squeeze()
+                figure.add_trace(
+                    go.Scatter(
+                        x=contour[:, 0],
+                        y=-contour[:, 1],
+                        text=str(bin),
+                        line_width=0,
+                        fill="toself",
+                        mode="lines",
+                        showlegend=False,
+                        hoverinfo="text",
+                        hoveron="fills",
+                        fillcolor=mpl.colors.to_hex(colors[color_i % len(colors)]),
+                    )
                 )
-            )
             color_i += 1
 
     figure.update_layout(
-        width=bins.shape[1] * 0.2,
-        height=bins.shape[0] * 0.2,
+        width=bins.shape[1] * scale,
+        height=bins.shape[0] * scale,
         xaxis=dict(showgrid=False, visible=False),
         yaxis=dict(showgrid=False, visible=False),
         margin=dict(l=0, r=0, t=0, b=0),
