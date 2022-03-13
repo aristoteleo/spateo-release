15667e1cb1c252cb3030baa5bbf679ea1f183822
Lioscro <phoenixter96@gmail.com>
2022-03-12 22:16:27 -0500

    nonrigid alignment

--- spateo/preprocessing/segmentation/align.py
+++ spateo/preprocessing/segmentation/align.py
@@ -1,5 +1,6 @@
 """Functions to refine staining and RNA alignments.
 """
+import math
 from typing import List, Optional
 
 import numpy as np
@@ -7,47 +8,26 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from anndata import AnnData
+from kornia.geometry.transform import thin_plate_spline as tps
 from tqdm import tqdm
+from typing_extensions import Literal
 
 from ...configuration import SKM
 from ...errors import PreprocessingError
 
 
 class AlignmentRefiner(nn.Module):
-    """Pytorch module to refine alignment between two images.
-    Performs Autograd on the affine transformation matrix.
-    """
-
-    def __init__(self, reference: np.ndarray, to_align: np.ndarray, theta: Optional[np.ndarray] = None):
+    def __init__(self, reference: np.ndarray, to_align: np.ndarray):
         if reference.dtype != np.dtype(bool) or to_align.dtype != np.dtype(bool):
             raise PreprocessingError("`AlignmentRefiner` only supports boolean arrays.")
-
         super().__init__()
         self.reference = torch.tensor(reference)[None][None].float()
         self.to_align = torch.tensor(to_align)[None][None].float()
-
         self.weight = torch.tensor(
             np.where(reference, reference.size / (2 * reference.sum()), reference.size / (2 * (~reference).sum()))
         )[None][None]
-
-        # Affine matrix
-        if theta is not None:
-            self.theta = nn.Parameter(torch.tensor(theta))
-        else:
-            self.theta = nn.Parameter(
-                torch.tensor(
-                    [
-                        [1.0, 0.0, 0.0],
-                        [0.0, 1.0, 0.0],
-                    ]
-                )
-            )
-
         self.__optimizer = None
         self.history = {}
-
-    def forward(self):
-        return self.transform(self.theta, self.to_align, train=True)
 
     def loss(self, pred):
         return torch.sum(self.weight * (pred - self.reference) ** 2) / self.weight.numel()
@@ -56,6 +36,9 @@ class AlignmentRefiner(nn.Module):
         if self.__optimizer is None:
             self.__optimizer = torch.optim.Adam(self.parameters())
         return self.__optimizer
+
+    def forward(self):
+        return self.transform(self.to_align, self.get_params(True), train=True)
 
     def train(self, n_epochs: int = 100):
         optimizer = self.optimizer()
@@ -73,11 +56,79 @@ class AlignmentRefiner(nn.Module):
                 loss.backward()
                 optimizer.step()
 
+    def get_params(self, train=False):
+        raise NotImplementedError()
+
+
+class NonRigidAlignmentRefiner(AlignmentRefiner):
+    """Pytorch module to refine alignment between two images by evaluating the
+    thin-plate-spline (TPS) for non-rigid alignment.
+    Performs Autograd on the displacement matrix between source and destination
+    points.
+    """
+
+    def __init__(self, reference: np.ndarray, to_align: np.ndarray, binsize: int = 1000):
+        super().__init__(reference, to_align)
+        self.src_points = torch.cartesian_prod(
+            torch.linspace(-1, 1, math.ceil(to_align.shape[1] / binsize)),
+            torch.linspace(-1, 1, math.ceil(to_align.shape[0] / binsize)),
+        )
+        self.displacement = nn.Parameter(torch.zeros(self.src_points.shape))
+
+    def get_params(self, train=False):
+        src_points, displacement = self.src_points, self.displacement
+        if not train:
+            src_points = src_points.detach().numpy()
+            displacement = displacement.detach().numpy()
+        return dict(src_points=src_points, displacement=displacement)
+
     @staticmethod
-    def transform(theta, x, train=False):
+    def transform(x, params, train=False):
         """This method should be used when applying the learned affine
         transformation to an arbitrary image.
         """
+        src_points, displacement = params["src_points"], params["displacement"]
+        dst_points = src_points + displacement
+        if not train:
+            src_points = torch.tensor(src_points).float()
+            dst_points = torch.tensor(dst_points).float()
+            x = torch.tensor(x)[None][None].float()
+        dst_points = dst_points.unsqueeze(0)
+        src_points = src_points.unsqueeze(0)
+        kernel_weights, affine_weights = tps.get_tps_transform(dst_points, src_points)
+        t = tps.warp_image_tps(x, src_points, kernel_weights, affine_weights).squeeze()
+        return t if train else t.detach().numpy()
+
+
+class RigidAlignmentRefiner(AlignmentRefiner):
+    """Pytorch module to refine alignment between two images.
+    Performs Autograd on the affine transformation matrix.
+    """
+
+    def __init__(self, reference: np.ndarray, to_align: np.ndarray, theta: Optional[np.ndarray] = None):
+        super().__init__(reference, to_align)
+        self.reference = torch.tensor(reference)[None][None].float()
+        self.to_align = torch.tensor(to_align)[None][None].float()
+
+        # Affine matrix
+        if theta is not None:
+            self.theta = nn.Parameter(torch.tensor(theta))
+        else:
+            self.theta = nn.Parameter(
+                torch.tensor(
+                    [
+                        [1.0, 0.0, 0.0],
+                        [0.0, 1.0, 0.0],
+                    ]
+                )
+            )
+
+    @staticmethod
+    def transform(x, params, train=False):
+        """This method should be used when applying the learned affine
+        transformation to an arbitrary image.
+        """
+        theta = params["theta"]
         if not train:
             theta = torch.tensor(theta).float()
             x = torch.tensor(x)[None][None].float()
@@ -85,16 +136,24 @@ class AlignmentRefiner(nn.Module):
         t = F.grid_sample(x, grid, align_corners=False)
         return t if train else t.detach().numpy()
 
-    def affine(self):
-        return self.theta.detach().numpy()
+    def get_params(self, train=False):
+        theta = self.theta
+        if not train:
+            theta = theta.detach().numpy()
+        return dict(theta=theta)
+
+
+MODULES = {"rigid": RigidAlignmentRefiner, "non-rigid": NonRigidAlignmentRefiner}
 
 
 def refine_alignment(
     adata: AnnData,
     stain_layer: str = SKM.STAIN_LAYER_KEY,
     rna_layer: str = SKM.UNSPLICED_LAYER_KEY,
+    mode: Literal["rigid", "non-rigid"] = "rigid",
     n_epochs: int = 100,
     transform_layers: Optional[List[str]] = None,
+    **kwargs,
 ):
     """Refine the alignment between the staining image and RNA coordinates.
 
@@ -111,7 +170,11 @@ def refine_alignment(
             `{rna_layer}_mask`. Otherwise, this will be taken as a literal.
         n_epochs: Number of epochs to run optimization
         transform_layers: Layers to transform and overwrite inplace.
+        **kwargs: Additional keyword arguments to pass to the Pytorch module.
     """
+    if mode not in MODULES.keys():
+        raise PreprocessingError('`mode` must be one of "rigid" and "non-rigid"')
+
     layer = SKM.gen_new_layer_key(stain_layer, SKM.MASK_SUFFIX)
     if layer not in adata.layers:
         layer = stain_layer
@@ -122,17 +185,17 @@ def refine_alignment(
         layer = rna_layer
     rna_mask = SKM.select_layer_data(adata, layer)
 
-    aligner = AlignmentRefiner(rna_mask, stain_mask)
+    module = MODULES[mode]
+    aligner = module(rna_mask, stain_mask, **kwargs)
     aligner.train(n_epochs)
-    theta = aligner.affine()
 
-    uns_alignment = {"theta": theta}
-    SKM.set_uns_spatial_attribute(adata, SKM.UNS_SPATIAL_ALIGNMENT_KEY, uns_alignment)
+    params = aligner.get_params()
+    SKM.set_uns_spatial_attribute(adata, SKM.UNS_SPATIAL_ALIGNMENT_KEY, params)
 
     if transform_layers:
         for layer in transform_layers:
             data = SKM.select_layer_data(adata, layer)
-            transformed = aligner.transform(theta, data)
+            transformed = aligner.transform(data, params)
             if data.dtype == np.dtype(bool):
                 transformed = transformed > 0.5
             SKM.set_layer_data(adata, layer, transformed)
