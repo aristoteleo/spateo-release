c68ef68d1d6808967ca63088dc87be34ab7e3c68
Jiajun Yao <yjj_zj@163.com>
2022-03-28 10:23:16 +0800

    update details

--- spateo/tools/TDR/weight/pick.py
+++ spateo/tools/TDR/weight/pick.py
@@ -13,8 +13,7 @@ def three_d_pick(
     key: str = "groups",
     picked_groups: Union[str, list] = None,
 ) -> MultiBlock:
-    """Pick the desired groups.
-    """
+    """Pick the desired groups."""
     # Check input mesh.
     mesh = multiblock2mesh(mesh=mesh, message=None) if isinstance(mesh, MultiBlock) else mesh.copy()
 
