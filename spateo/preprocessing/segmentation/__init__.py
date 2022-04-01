810eabd1b67e23ad12da2b801b1bcf3eb7a40a0e
Lioscro <phoenixter96@gmail.com>
2022-04-01 16:25:12 -0400

    add deepcell

--- spateo/preprocessing/segmentation/__init__.py
+++ spateo/preprocessing/segmentation/__init__.py
@@ -5,6 +5,7 @@ from .align import refine_alignment
 from .benchmark import compare
 from .density import merge_densities, segment_densities
 from .external.cellpose import cellpose
+from .external.deepcell import deepcell
 from .external.stardist import stardist
 from .icell import mask_cells_from_stain, mask_nuclei_from_stain, score_and_mask_pixels
 from .label import (
