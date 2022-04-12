4dffac256168234a325708ffe0e7a2d390370747
Xiaojie Qiu <xqiu@uw.edu>
2022-04-12 16:51:18 -0400

    update KernelInterpolation related code

--- spateo/tools/kernel_interpolation.py
+++ spateo/tools/kernel_interpolation.py
@@ -2,70 +2,71 @@
 Todo:
     * @Xiaojieqiu: update with Google style documentation, function typings, tests
 """
-import numpy as np
+from typing import List, Optional, Tuple, Union
 
+from ..logging import logger_manager as lm
 from .deep_interpolation import *
+from .utils import in_hull, polyhull
 
 
-def interpolation_SparseVFC(adata, genes=None, grid_num=50, lambda_=0.02, lstsq_method="scipy", **kwargs):
-    """
-    predict missing location’s gene expression and learn a continuous gene expression pattern over space
+def KernelInterpolation(
+    adata: AnnData,
+    genes: Tuple[None, List] = None,
+    grid_num: List = [50, 50, 50],
+    lambda_: float = 0.02,
+    lstsq_method: str = "scipy",
+    **kwargs,
+) -> AnnData:
+    """Learn a continuous mapping from space to gene expression pattern with the Kernel method (sparseVFC).
 
-    Parameters
-    ----------
-        adata: :class:`~anndata.AnnData`
-            AnnData object that contains spatial (numpy.ndarray) in the `obsm` attribute.
-        genes: `list` (default None)
-            Gene list that needs to interpolate.
-        grid_num: 'int' (default 50)
-            Number of grid to generate. Default is 50. Must be non-negative.
-        lambda_: 'float' (default: 0.02)
-            Represents the trade-off between the goodness of data fit and regularization. Larger Lambda_ put more weights
+    Args:
+        adata: AnnData object that contains spatial (numpy.ndarray) in the `obsm` attribute.
+        genes: Gene list that needs to interpolate.
+        grid_num: Number of grid to generate. Default is 50 for each dimension. Must be non-negative.
+        lambda_: Represents the trade-off between the goodness of data fit and regularization. Larger Lambda_ put more weights
             on regularization.
-        lstsq_method: 'str' (default: `scipy`)
-           The name of the linear least square solver, can be either 'scipy` or `douin`.
-        **kwargs：
-        Additional parameters that will be passed to SparseVFC function.
+        lstsq_method: The name of the linear least square solver, can be either 'scipy` or `douin`.
+        **kwargs: Additional parameters that will be passed to SparseVFC function.
 
-    Returns
-    -------
-    Res: 'dict'
-        A dictionary which contains:
-            X: Current location.
-            valid_ind: The indices of cells that have finite expression values.
-            X_ctrl: Sample control points of current location.
-            ctrl_idx: Indices for the sampled control points.
-            Y: expression estimates in delta t.
-            beta: Parameter of the Gaussian Kernel for the kernel matrix (Gram matrix).
-            V: Prediction of expression of X.
-            C: Finite set of the coefficients for the
-            P: Posterior probability Matrix of inliers.
-            VFCIndex: Indexes of inliers found by sparseVFC.
-            sigma2: Energy change rate.
-            grid: Grid of current location.
-            grid_V: Prediction of expression of the grid.
-            iteration: Number of the last iteration.
-            tecr_vec: Vector of relative energy changes rate comparing to previous step.
-            E_traj: Vector of energy at each iteration,
-        where V = f(X), P is the posterior probability and VFCIndex is the indexes of inliers found by sparseVFC.
-        Note that V = `con_K(Grid, X_ctrl, beta).dot(C)` gives the prediction of expression on Grid (but can also be any
-        point in the gene expression location space).
-
+    Returns:
+        interp_adata: an anndata object that has interpolated expression. The row of the adata object is a grid point
+        within the convex hull formed by the input data points while each column corresponds a gene whose expression
+        values are interpolated.
     """
+
     from dynamo.vectorfield.scVectorField import SparseVFC
+
+    lm.info("Learn a continuous mapping from space to gene expression pattern")
+    lm.log_time()
 
     X, V = adata.obsm["spatial"], adata[:, genes].X
 
     # Generate grid
+    lm.main_info("Generate grid...")
     min_vec, max_vec = (
         X.min(0),
         X.max(0),
     )
     min_vec = min_vec - 0.01 * np.abs(max_vec - min_vec)
     max_vec = max_vec + 0.01 * np.abs(max_vec - min_vec)
-    Grid_list = np.meshgrid(*[np.linspace(i, j, grid_num) for i, j in zip(min_vec, max_vec)])
+    Grid_list = np.meshgrid(*[np.linspace(i, j, k) for i, j, k in zip(min_vec, max_vec, grid_num)])
     Grid = np.array([i.flatten() for i in Grid_list]).T
+
+    lm.main_info("Creating a Convex Hull...")
+    hull = polyhull(X[:, 0], X[:, 1], X[:, 2])
+
+    lm.main_info("Identify grid points within the Convex Hull...")
+    grid_in_hull = in_hull(Grid, hull.points[hull.vertices, :])
 
     res = SparseVFC(X, V, Grid, lambda_=lambda_, lstsq_method=lstsq_method, **kwargs)
 
-    return res
+    lm.main_info("Creating an adata object with the interpolated expression...")
+    interp_adata = AnnData(
+        X=res["grid_V"][grid_in_hull],
+        obsm={"spatial": res["grid"][grid_in_hull]},
+        var=adata[:, genes].var,
+    )
+
+    lm.finish_progress(progress_name="KernelInterpolation")
+
+    return interp_adata
