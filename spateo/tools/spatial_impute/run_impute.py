d53f80ce1c57a1e8459a7e916ca3e3834f67a0ad
Daniel Y. Zhu <danielyumengzhu@gmail.com>
2022-08-31 14:14:09 -0400

    Updated visualization following run_denoise_impute

--- spateo/tools/spatial_impute/run_impute.py
+++ spateo/tools/spatial_impute/run_impute.py
@@ -17,6 +17,7 @@ from ...plotting.static.space import space
 def run_denoise_impute(
     adata: AnnData,
     spatial_key: str = "spatial",
+    group_key: Union[None, str] = None,
     device: str = "cpu",
     to_visualize: Union[None, str, List[str]] = None,
     clip: Union[None, float] = None,
@@ -31,6 +32,9 @@ def run_denoise_impute(
             AnnData object to model
         spatial_key : str, default 'spatial'
             Key in .obsm where x- and y-coordinates are stored
+        group_key : optional str
+            Key in .obs where labels are stored. If given, can plot categories alongside gene expression,
+            i.e. to visualize marker genes with respect to label boundaries.
         device : str, default 'cpu'
             Options: 'cpu', 'cuda:_', to run on either CPU or GPU. If running on GPU, provide the label of the device
             to run on.
@@ -52,65 +56,14 @@ def run_denoise_impute(
     if to_visualize is not None:
         for feat in to_visualize:
             # Generate two plots: one for observed data and one for imputed:
-            print(f"{feat} Observed")
-            size = 3000 / adata_orig.n_obs
-            space(adata_orig, color=feat, cmap="magma", figsize=(5, 5), dpi=300, pointsize=size, alpha=0.9)
+            # To be able to visualize side-by-side, temporarily append the smoothed feature to the original under a
+            # different index in .var:
+            adata_to_vis = adata_orig.copy()
+            adata_to_vis[:, feat + "_gcn"] = adata_rex[:, feat]
 
-            print(f"{feat} Imputed")
-            size = 3000 / adata_orig.n_obs
-            space(adata_rex, color=feat, cmap="magma", figsize=(5, 5), dpi=300, pointsize=size, alpha=0.9)
-
-            """
-            # Generate two plots: one for observed data and one for imputed:
-            to_plot_orig = adata_orig[:, feat].X
-            if sp.issparse(to_plot_orig):
-                to_plot_orig = to_plot_orig.toarray()
-            to_plot_rex = adata_rex[:, feat].X
-            # For visualization, set max colormap value to the 99th percentile values:
-            orig_vmax = np.percentile(to_plot_orig, 99)
-            rex_vmax = np.percentile(to_plot_rex, 99)
-
-            fig, ax = plt.subplots(1, 1, figsize=(7.5, 7.5), constrained_layout=True)
-            size = 100000 / adata.n_obs
-            scatterplot = ax.scatter(
-                adata.obsm[spatial_key][:, 0],
-                adata.obsm[spatial_key][:, 1],
-                c=to_plot_orig,
-                cmap=cmap,
-                vmin=0,
-                vmax=orig_vmax,
-                s=size,
-                alpha=1.0,
-            )
-            ax.set_aspect("equal", "datalim")
-            ax.set_title(
-                f"{feat.title()} Observed",
-                fontsize=14,
-                fontweight="bold",
-            )
-            ax.set_ylim(ax.get_ylim()[::-1])
-            cbar = fig.colorbar(scatterplot)
-            # https://stackoverflow.com/questions/62812792/adding-colorbar-to-scatterplot-after-loop
-            plt.show()
-
-            fig, ax = plt.subplots(1, 1, figsize=(7.5, 7.5), constrained_layout=True)
-            size = 100000 / adata.n_obs
-            scatterplot = ax.scatter(
-                adata.obsm[spatial_key][:, 0],
-                adata.obsm[spatial_key][:, 1],
-                c=to_plot_rex,
-                cmap=cmap,
-                vmin=0,
-                vmax=rex_vmax,
-                s=size,
-                alpha=1.0,
-            )
-            ax.set_aspect("equal", "datalim")
-            ax.set_title(
-                f"{feat.title()} Enhanced",
-                fontsize=14,
-                fontweight="bold",
-            )
-            ax.set_ylim(ax.get_ylim()[::-1])
-            cbar = fig.colorbar(scatterplot)
-            plt.show()"""
+            size = 0.35 if len(adata_to_vis) < 1000 else 0.1
+            if group_key is not None:
+                space(adata_to_vis, color=[feat, feat + "_gcn", group_key], ncols=2, cmap=cmap, dpi=300, pointsize=size,
+                      alpha=1)
+            else:
+                space(adata_to_vis, color=[feat, feat + "_gcn"], ncols=2, cmap=cmap, dpi=300, pointsize=size, alpha=1)
