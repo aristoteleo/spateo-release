df4c17638358275ebd1530a972b6c1ea88a665d7
Jiajun Yao <yjj_zj@163.com>
2022-09-06 10:14:30 +0800

    delete registered colormap

--- spateo/configuration.py
+++ spateo/configuration.py
@@ -239,39 +239,6 @@ class SpateoAdataKeyManager:
 
 SKM = SpateoAdataKeyManager
 
-fire_cmap = matplotlib.colors.LinearSegmentedColormap.from_list("fire", colorcet.fire)
-darkblue_cmap = matplotlib.colors.LinearSegmentedColormap.from_list("darkblue", colorcet.kbc)
-darkgreen_cmap = matplotlib.colors.LinearSegmentedColormap.from_list("darkgreen", colorcet.kgy)
-darkred_cmap = matplotlib.colors.LinearSegmentedColormap.from_list(
-    "darkred", colors=colorcet.linear_kry_5_95_c72[:192], N=256
-)
-darkpurple_cmap = matplotlib.colors.LinearSegmentedColormap.from_list("darkpurple", colorcet.linear_bmw_5_95_c89)
-# add gkr theme
-div_blue_black_red_cmap = matplotlib.colors.LinearSegmentedColormap.from_list(
-    "div_blue_black_red", colorcet.diverging_gkr_60_10_c40
-)
-# add RdBu_r theme
-div_blue_red_cmap = matplotlib.colors.LinearSegmentedColormap.from_list(
-    "div_blue_red", colorcet.diverging_bwr_55_98_c37
-)
-# add glasbey_bw for cell annotation in white background
-glasbey_white_cmap = matplotlib.colors.LinearSegmentedColormap.from_list("glasbey_white", colorcet.glasbey_bw_minc_20)
-# add glasbey_bw_minc_20_maxl_70 theme for cell annotation in dark background
-glasbey_dark_cmap = matplotlib.colors.LinearSegmentedColormap.from_list(
-    "glasbey_dark", colorcet.glasbey_bw_minc_20_maxl_70
-)
-
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
-    plt.register_cmap("fire", fire_cmap)
-    plt.register_cmap("darkblue", darkblue_cmap)
-    plt.register_cmap("darkgreen", darkgreen_cmap)
-    plt.register_cmap("darkred", darkred_cmap)
-    plt.register_cmap("darkpurple", darkpurple_cmap)
-    plt.register_cmap("div_blue_black_red", div_blue_black_red_cmap)
-    plt.register_cmap("div_blue_red", div_blue_red_cmap)
-    plt.register_cmap("glasbey_white", glasbey_white_cmap)
-    plt.register_cmap("glasbey_dark", glasbey_dark_cmap)
 
 _themes = {
     "fire": {
