0235b01ad3da41501b6282bca8cdb7825c2b3666
Jiajun Yao <yjj_zj@163.com>
2022-09-11 00:25:37 +0800

    add docs to glm_fit

--- spateo/plotting/static/glm.py
+++ spateo/plotting/static/glm.py
@@ -20,10 +20,10 @@ def glm_fit(
     adata: AnnData,
     gene: Union[str, list] = None,
     feature_x: str = None,
-    feature_y: Optional[str] = None,
+    feature_y: Optional[str] = "expression",
     feature_fit: Optional[str] = "mu",
     glm_key: str = "glm_degs",
-    lowess_smooth: bool = True,
+    lowess: bool = True,
     frac: float = 0.1,
     point_size: float = 1,
     point_color: str = "skyblue",
@@ -31,14 +31,48 @@ def glm_fit(
     line_color: str = "black",
     ax_size: Union[tuple, list] = (3, 3),
     background_color: str = "white",
-    ncol: int = 4,
+    ncols: int = 4,
     save_show_or_return: Literal["save", "show", "return", "both", "all"] = "show",
     save_kwargs: Optional[dict] = None,
     **kwargs,
 ):
+    """
+    Visualize glm_degs result.
+
+    Args:
+        adata: An Anndata object contain glm_degs result in ``.uns[glm_key]``.
+        gene: A gene name or a list of genes that will be used to plot.
+        feature_x: The key in ``.uns[glm_key]['correlation'][gene]`` that corresponds to the independent variables, such as ``'torsion'``, etc.
+        feature_y: The key in ``.uns[glm_key]['correlation'][gene]`` that corresponds to the dependent variables, such as ``'expression'``, etc.
+        feature_fit: The key in ``.uns[glm_key]['correlation'][gene]`` that corresponds to the fitted values, such as ``'mu'``, etc.
+        glm_key: The key in ``.uns`` that corresponds to the glm_degs result.
+        lowess: Locally Weighted Scatter-plot Smoothing. Whether to use the lowess function on the feature_y value.
+        frac: Between 0 and 1. The fraction of the data used when estimating each feature_y-value. Only valid when ``lowess`` is True.
+        point_size: The scale of the feature_y point size.
+        point_color: The color of the feature_y point.
+        line_size: The scale of the fitted line width.
+        line_color: The color of the fitted line.
+        ax_size: The width and height of each ax.
+        background_color: The background color of the figure.
+        ncols: Number of columns for the figure.
+        save_show_or_return: If ``'both'``, it will save and plot the figure at the same time.
+
+                             If ``'all'``, the figure will be saved, displayed and the associated axis and other object will be return.
+        save_kwargs: A dictionary that will be passed to the save_fig function.
+
+                     By default, it is an empty dictionary and the save_fig function will use the ``{"path": None, "prefix": 'scatter',
+                     "dpi": None, "ext": 'pdf', "transparent": True, "close": True, "verbose": True}`` as its parameters.
+
+                     Otherwise, you can provide a dictionary that properly modify those keys according to your needs.
+        **kwargs: Additional parameters that will be passed into the ``statsmodels.nonparametric.smoothers_lowess.lowess`` function.
+
+    """
 
     assert not (gene is None), "``gene`` cannot be None."
     assert not (feature_x is None), "``feature_x`` cannot be None."
+    assert not (
+        feature_y is None and feature_fit is None
+    ), "``feature_y`` and ``feature_fit`` cannot be None at the same time."
     assert (
         glm_key in adata.uns
     ), f"``glm_key`` does not exist in adata.uns, please replace ``glm_key`` or run st.tl.glm_degs(key_added={glm_key})."
@@ -46,26 +80,26 @@ def glm_fit(
     genes = list(gene) if isinstance(gene, list) else [gene]
     genes_data = [adata.uns[glm_key]["correlation"][g].copy() for g in genes]
 
-    ncol = len(genes) if len(genes) < ncol else ncol
-    nrow = math.ceil(len(genes) / ncol)
-    fig = plt.figure(figsize=(ax_size[0] * ncol, ax_size[1] * nrow))
+    ncols = len(genes) if len(genes) < ncols else ncols
+    nrows = math.ceil(len(genes) / ncols)
+    fig = plt.figure(figsize=(ax_size[0] * ncols, ax_size[1] * nrows))
 
     axes_list = []
     for i, data in enumerate(genes_data):
         data.sort_values(by=feature_x, ascending=True, axis=0, inplace=True)
         feature_x_values = np.asarray(data[feature_x]).flatten()
 
-        ax = plt.subplot(nrow, ncol, i + 1)
+        ax = plt.subplot(nrows, ncols, i + 1)
         ax.set_title(f"Gene: {genes[i]}")
         if not (feature_y is None):
             feature_y_values = np.asarray(data[feature_y]).flatten()
-            if lowess_smooth:
+            if lowess:
                 tmp = sm.nonparametric.lowess(
-                    feature_y_values, feature_x_values, frac=frac, is_sorted=True, return_sorted=True
+                    feature_y_values, feature_x_values, frac=frac, is_sorted=True, return_sorted=True, **kwargs
                 )
                 f = interp1d(tmp[:, 0], tmp[:, 1], bounds_error=False)
                 feature_y_values = f(feature_x_values)
-            ax.scatter(feature_x_values, feature_y_values, c=point_color, s=point_size, **kwargs)
+            ax.scatter(feature_x_values, feature_y_values, c=point_color, s=point_size)
 
         if not (feature_fit is None):
             feature_fit_values = np.asarray(data[feature_fit]).flatten()
@@ -76,7 +110,7 @@ def glm_fit(
 
     fig.supxlabel(feature_x)
     fig.supylabel(feature_y)
-    added_pad = nrow * 0.1 if ncol * 2 < nrow else ncol * 0.2
+    added_pad = nrows * 0.1 if ncols * 2 < nrows else ncols * 0.2
     plt.tight_layout(pad=1 + added_pad)
     return save_return_show_fig_utils(
         save_show_or_return=save_show_or_return,
