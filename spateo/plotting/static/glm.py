31407f2462175c782e2841d8483da74123101e5f
Jiajun Yao <yjj_zj@163.com>
2022-09-10 12:59:46 +0800

    update glm fitted line

--- spateo/plotting/static/glm.py
+++ spateo/plotting/static/glm.py
@@ -11,16 +11,15 @@ try:
 except ImportError:
     from typing_extensions import Literal
 
-import seaborn as sns
-
 from .utils import save_return_show_fig_utils
 
 
-def glm_abline(
+def glm_fit(
     adata: AnnData,
     gene: Union[str, list] = None,
     feature_x: str = None,
-    feature_y: str = None,
+    feature_y: Optional[str] = None,
+    feature_fit: Optional[str] = "mu",
     glm_key: str = "glm_degs",
     point_size: float = 1,
     point_color: str = "skyblue",
@@ -36,7 +35,6 @@ def glm_abline(
 
     assert not (gene is None), "``gene`` cannot be None."
     assert not (feature_x is None), "``feature_x`` cannot be None."
-    assert not (feature_y is None), "``feature_y`` cannot be None."
     assert (
         glm_key in adata.uns
     ), f"``glm_key`` does not exist in adata.uns, please replace ``glm_key`` or run st.tl.glm_degs(key_added={glm_key})."
@@ -50,12 +48,16 @@ def glm_abline(
 
     axes_list = []
     for i, data in enumerate(genes_data):
-        ax = plt.subplot(nrow, ncol, i + 1)
-        ax.set_title(f"Model Fit Plot of  {genes[i]}")
-        ax.scatter(data[feature_x], data[feature_y], c=point_color, s=point_size, **kwargs)
+        data.sort_values(by=feature_x, ascending=True, axis=0, inplace=True)
 
-        fit_line = sm.OLS(data[feature_y], sm.add_constant(data[feature_x], prepend=True)).fit()
-        abline_plot(model_results=fit_line, ax=ax, color=line_color, linewidth=line_size)
+        ax = plt.subplot(nrow, ncol, i + 1)
+        ax.set_title(f"Gene: {genes[i]}")
+        if not (feature_y is None):
+            ax.scatter(data[feature_x], data[feature_y], c=point_color, s=point_size, **kwargs)
+        if not (feature_fit is None):
+            ax.plot(data[feature_x], data[feature_fit], color=line_color, linewidth=line_size)
+        # fit_line = sm.OLS(data[feature_y], sm.add_constant(data[feature_x], prepend=True)).fit()
+        # abline_plot(model_results=fit_line, ax=ax, color=line_color, linewidth=line_size)
         axes_list.append(ax)
 
     fig.supxlabel(feature_x)
