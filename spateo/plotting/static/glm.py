a851aaafa0c17cd0161807bd9c25f3e24de04301
Jiajun Yao <yjj_zj@163.com>
2022-09-10 23:47:20 +0800

    update glm fitted line

--- spateo/plotting/static/glm.py
+++ spateo/plotting/static/glm.py
@@ -2,8 +2,10 @@ import math
 from typing import Optional, Union
 
 import matplotlib.pyplot as plt
+import numpy as np
 import statsmodels.api as sm
 from anndata import AnnData
+from scipy.interpolate import interp1d
 from statsmodels.graphics.api import abline_plot
 
 try:
@@ -21,6 +23,8 @@ def glm_fit(
     feature_y: Optional[str] = None,
     feature_fit: Optional[str] = "mu",
     glm_key: str = "glm_degs",
+    lowess_smooth: bool = True,
+    frac: float = 0.1,
     point_size: float = 1,
     point_color: str = "skyblue",
     line_size: float = 1,
@@ -49,20 +53,31 @@ def glm_fit(
     axes_list = []
     for i, data in enumerate(genes_data):
         data.sort_values(by=feature_x, ascending=True, axis=0, inplace=True)
+        feature_x_values = np.asarray(data[feature_x]).flatten()
 
         ax = plt.subplot(nrow, ncol, i + 1)
         ax.set_title(f"Gene: {genes[i]}")
         if not (feature_y is None):
-            ax.scatter(data[feature_x], data[feature_y], c=point_color, s=point_size, **kwargs)
+            feature_y_values = np.asarray(data[feature_y]).flatten()
+            if lowess_smooth:
+                tmp = sm.nonparametric.lowess(
+                    feature_y_values, feature_x_values, frac=frac, is_sorted=True, return_sorted=True
+                )
+                f = interp1d(tmp[:, 0], tmp[:, 1], bounds_error=False)
+                feature_y_values = f(feature_x_values)
+            ax.scatter(feature_x_values, feature_y_values, c=point_color, s=point_size, **kwargs)
+
         if not (feature_fit is None):
-            ax.plot(data[feature_x], data[feature_fit], color=line_color, linewidth=line_size)
-        # fit_line = sm.OLS(data[feature_y], sm.add_constant(data[feature_x], prepend=True)).fit()
-        # abline_plot(model_results=fit_line, ax=ax, color=line_color, linewidth=line_size)
+            feature_fit_values = np.asarray(data[feature_fit]).flatten()
+            ax.plot(feature_x_values, feature_fit_values, color=line_color, linewidth=line_size)
+            # fit_line = sm.OLS(data[feature_y], sm.add_constant(data[feature_x], prepend=True)).fit()
+            # abline_plot(model_results=fit_line, ax=ax, color=line_color, linewidth=line_size)
         axes_list.append(ax)
 
     fig.supxlabel(feature_x)
     fig.supylabel(feature_y)
-    plt.tight_layout()
+    added_pad = nrow * 0.1 if ncol * 2 < nrow else ncol * 0.2
+    plt.tight_layout(pad=1 + added_pad)
     return save_return_show_fig_utils(
         save_show_or_return=save_show_or_return,
         show_legend=False,
