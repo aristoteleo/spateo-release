d54070546b8e3a469962f34b4ac5ba5d3aba583b
Daniel Y. Zhu <danielyumengzhu@gmail.com>
2022-10-15 14:38:30 -0400

    Removing cell_interaction folder
    In preparation for refactoring (to dodge merge conflicts)

--- spateo/tools/cell_interaction/__init__.py
+++
@@ -1,8 +0,0 @@
-from .cci_two_cluster import find_cci_two_group
-from .cell_communication import niches, predict_ligand_activities, predict_target_genes
-from .spatial_regression import (
-    Category_Interpreter,
-    Ligand_Lagged_Interpreter,
-    Niche_Interpreter,
-    Niche_LR_Interpreter,
-)
