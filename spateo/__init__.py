6e7d547b54a329a63d677d51c81cb5b00a7a92e9
Jiajun Yao <yjj_zj@163.com>
2022-11-10 00:36:16 +0800

    pre-commit run --all

--- spateo/__init__.py
+++ spateo/__init__.py
@@ -6,6 +6,6 @@ from .get_version import get_version
 __version__ = get_version(__file__)
 del get_version
 
-from . import cs, dd, io, pl, pp, tdr, tl, sample_data
+from . import cs, dd, io, pl, pp, sample_data, tdr, tl
 from .configuration import config
 from .data_io import *
