c50e63d1f9a3c78f6ade790dee7639029cffc0fc
Jiajun Yao <yjj_zj@163.com>
2022-11-10 00:35:40 +0800

    update sample data

--- spateo/sample_data.py
+++ spateo/sample_data.py
@@ -1,5 +1,6 @@
 import ntpath
 import os
+import shutil
 from pathlib import Path
 from typing import Optional, Tuple, Union
 from urllib.request import urlretrieve
@@ -26,28 +27,30 @@ def download_data(url: str, file_path: Optional[str] = None, dir_name: str = "./
     lm.main_info("Downloading data to " + file_path)
 
     if not os.path.exists(file_path):
-        if not os.path.exists("./data/"):
-            os.mkdir("data")
+        if not os.path.exists(dir_name):
+            Path(dir_name).mkdir(parents=True, exist_ok=True)
 
         # download the data
+        print(url)
         urlretrieve(url, file_path, reporthook=lm.get_main_logger().request_report_hook)
 
     return file_path
 
 
-def get_adata(url: str, filename: Optional[str] = None) -> AnnData:
+def get_adata(url: str, filename: Optional[str] = None, dir_name: str = "./data") -> AnnData:
     """Download example data to local folder.
 
     Args:
         url: url that deposits the data.
         filename: file name that will store the data locally.
+        dir_name: name of the directory.
 
     Returns:
         adata: :class:`~anndata.AnnData`
             an Annodata object.
     """
 
-    file_path = download_data(url, filename)
+    file_path = download_data(url=url, file_path=filename, dir_name=dir_name)
     if Path(file_path).suffixes[-1][1:] == "loom":
         adata = read_loom(filename=file_path)
     elif Path(file_path).suffixes[-1][1:] == "h5ad":
@@ -59,29 +62,52 @@ def get_adata(url: str, filename: Optional[str] = None) -> AnnData:
 
 
 def drosophila(
-    url="https://www.dropbox.com/s/bvstb3en5kc6wui/E7-9h_cellbin_tdr_v2.h5ad?dl=0",
-    filename="drosophila_E7-9h_v2.h5ad",
+    filename: str = "E7-9h_cellbin_tdr_v2.h5ad",
+    dir_name: str = "./data",
 ):
-    """The E8-10 whole-body data of drosophila of aligned serial slices with tissue type annotations.
+    """Multiple drosophila spatial transcriptome data.
 
     Args:
-        url: url that deposits the data. Available ``url`` are:
+        filename: file name of the data.  Available ``filename`` are:
 
-                * ``E7-9h_cellbin_tdr_v1.h5ad``: ``https://www.dropbox.com/s/ow8xkge0538309a/E7-9h_cellbin_tdr_v1.h5ad?dl=0``
-                * ``E7-9h_cellbin_tdr_v2.h5ad``: ``https://www.dropbox.com/s/bvstb3en5kc6wui/E7-9h_cellbin_tdr_v2.h5ad?dl=0``
-                * ``E7-9h_cellbin_tdr_v2_midgut.h5ad``: ``https://www.dropbox.com/s/q020zgxxemxl7j4/E7-9h_cellbin_tdr_v2_midgut.h5ad?dl=0``
-                * ``E7-9h_cellbin_tdr_v3_midgut.h5ad``: ``https://www.dropbox.com/s/cz2nqpmoc3oo5f3/E7-9h_cellbin_tdr_v3_midgut.h5ad?dl=0``
-                * ``E9-10h_cellbin_tdr_v1.h5ad``: ``https://www.dropbox.com/s/q2l8mqpn7qvz2xr/E9-10h_cellbin_tdr_v1.h5ad?dl=0``
-                * ``E9-10h_cellbin_tdr_v2.h5ad``: ``https://www.dropbox.com/s/q02sx6acvcqaf35/E9-10h_cellbin_tdr_v2.h5ad?dl=0``
-                * ``E9-10h_cellbin_tdr_v2_midgut.h5ad``: ``https://www.dropbox.com/s/we2fkpd1p3ww33f/E9-10h_cellbin_tdr_v2_midgut.h5ad?dl=0``
-                * ``E9-10h_cellbin_tdr_v2_CNS.h5ad``: ``https://www.dropbox.com/s/a7bllwm760dmda6/E9-10h_cellbin_tdr_v2_CNS.h5ad?dl=0``
-        filename: file name that will store the data locally.
+                * ``E7-9h_cellbin_tdr_v1.h5ad``
+                * ``E7-9h_cellbin_tdr_v2.h5ad``
+                * ``E7-9h_cellbin_tdr_v2_midgut.h5ad``
+                * ``E7-9h_cellbin_tdr_v3_midgut.h5ad``
+                * ``E7-9h_cellbin_h5ad.zip``
+                * ``E7-9h_bin20_h5ad.zip``
+                * ``E9-10h_cellbin_tdr_v1.h5ad``
+                * ``E9-10h_cellbin_tdr_v2.h5ad``
+                * ``E9-10h_cellbin_tdr_v2_midgut.h5ad``
+                * ``E9-10h_cellbin_tdr_v2_CNS.h5ad``
+        dir_name: dir path that will store the data locally.
     Returns:
         Returns `adata` object
     """
-    adata = get_adata(url, filename)
+    url_dict = {
+        "E7-9h_cellbin_tdr_v1.h5ad": "https://www.dropbox.com/s/ow8xkge0538309a/E7-9h_cellbin_tdr_v1.h5ad?dl=0",
+        "E7-9h_cellbin_tdr_v2.h5ad": "https://www.dropbox.com/s/bvstb3en5kc6wui/E7-9h_cellbin_tdr_v2.h5ad?dl=0",
+        "E7-9h_cellbin_tdr_v2_midgut.h5ad": "https://www.dropbox.com/s/q020zgxxemxl7j4/E7-9h_cellbin_tdr_v2_midgut.h5ad?dl=0",
+        "E7-9h_cellbin_tdr_v3_midgut.h5ad": "https://www.dropbox.com/s/cz2nqpmoc3oo5f3/E7-9h_cellbin_tdr_v3_midgut.h5ad?dl=0",
+        "E7-9h_cellbin_h5ad.zip": "https://www.dropbox.com/s/dsgyc10q5s58ill/cellbin_h5ad.zip?dl=0",
+        "E7-9h_bin20_h5ad.zip": "https://www.dropbox.com/s/f3c635r4ro4zsmj/bin20_h5ad.zip?dl=0",
+        "E9-10h_cellbin_tdr_v1.h5ad": "https://www.dropbox.com/s/q2l8mqpn7qvz2xr/E9-10h_cellbin_tdr_v1.h5ad?dl=0",
+        "E9-10h_cellbin_tdr_v2.h5ad": "https://www.dropbox.com/s/q02sx6acvcqaf35/E9-10h_cellbin_tdr_v2.h5ad?dl=0",
+        "E9-10h_cellbin_tdr_v2_midgut.h5ad": "https://www.dropbox.com/s/we2fkpd1p3ww33f/E9-10h_cellbin_tdr_v2_midgut.h5ad?dl=0",
+        "E9-10h_cellbin_tdr_v2_CNS.h5ad": "https://www.dropbox.com/s/a7bllwm760dmda6/E9-10h_cellbin_tdr_v2_CNS.h5ad?dl=0",
+    }
+    if filename.endswith(".h5ad") or filename.endswith(".loom"):
+        adata = get_adata(url=url_dict[filename], filename=filename, dir_name=dir_name)
+        return adata
+    elif filename.endswith(".zip"):
+        extract_dir = os.path.join(dir_name, filename[:-4])
+        Path(extract_dir).mkdir(parents=True, exist_ok=True)
 
-    return adata
+        file_path = download_data(url=url_dict[filename], file_path=filename)
+        shutil.unpack_archive(file_path, extract_dir)
+
+        adata_list = [read_h5ad(filename=filename) for root, dirs, files in os.walk(extract_dir) for filename in files]
+        return adata_list
 
 
 def axolotl_2DPI(
