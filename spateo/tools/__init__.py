e6d91883ac083817a3812a3ef9ad5867234ce925
Daniel Y. Zhu <danielyumengzhu@gmail.com>
2022-11-19 17:40:13 -0500

    Update init for tools

--- spateo/tools/__init__.py
+++ spateo/tools/__init__.py
@@ -18,7 +18,9 @@ from .cluster_degs import (
 from .cluster_lasso import *
 from .coarse_align import AffineTrans, align_slices_pca, pca_align, procrustes
 from .find_neighbors import (
+    construct_geodesic_distance_matrix,
     construct_nn_graph,
+    construct_spatial_distance_matrix,
     generate_spatial_weights_fixed_nbrs,
     generate_spatial_weights_fixed_radius,
     weighted_expr_neighbors_graph,
