Found/fixed major issue in normalization

spateo/preprocessing/normalize.py
@@ -42,8 +42,10 @@ def _normalize_data(X, counts, after=None, copy=False, rows=True, round=False):
     counts_greater_than_zero = counts[counts > 0]
 
     after = np.median(counts_greater_than_zero, axis=0) if after is None else after
+
     counts += counts == 0
     counts = counts / after
+
     if scipy.sparse.issparse(X):
         sparsefuncs.inplace_row_scale(X, 1 / counts)
     elif isinstance(counts, np.ndarray):
@@ -104,6 +106,7 @@ def normalize_total(
         Returns dictionary with normalized copies of `adata.X` and `adata.layers` or updates `adata` with normalized
         version of the original `adata.X` and `adata.layers`, depending on `inplace`.
     """
+
     logger = lm.get_main_logger()
 
     if copy:
@@ -134,7 +137,6 @@ def normalize_total(
         nearest_power = 10**power
         target_sum = nearest_power
 
-    gene_subset = None
     msg = "Normalizing counts per cell..."
     if exclude_highly_expressed:
         counts_per_cell = X.sum(axis=1)  # original counts per cell
@@ -153,8 +155,22 @@ def normalize_total(
         counts_per_cell = X.sum(axis=1)
 
     if norm_factor is not None:
+        # Scale counts by dividing by the found scaling factors:
         norm_factor = norm_factor.reshape(-1, 1)
-        counts_per_cell = np.multiply(counts_per_cell, norm_factor)
+        scale_factor = np.multiply(counts_per_cell, norm_factor)
+        scale_factor = np.ravel(scale_factor)
+
+        if issubclass(X.dtype.type, (int, np.integer)):
+            X = X.astype(np.float32)
+        if scipy.sparse.issparse(X):
+            sparsefuncs.inplace_row_scale(X, scale_factor)
+        elif isinstance(scale_factor, np.ndarray):
+            np.divide(X, scale_factor[:, None], out=X)
+        else:
+            X = np.divide(X, scale_factor[:, None])
+
+        # Get new counts per cell:
+        counts_per_cell = X.sum(axis=1)
 
     # logger.info(msg)
     counts_per_cell = np.ravel(counts_per_cell)

spateo/tools/ST_modeling/MuSIC_downstream.py
@@ -13,6 +13,7 @@ import argparse
 import itertools
 import math
 import os
+import pickle
 import re
 import sys
 from collections import Counter
@@ -752,11 +753,11 @@ class MuSIC_Interpreter(MuSIC):
 
     def sender_receiver_effect_deg_detection(
         self,
-        target: Optional[str] = None,
         diff_sending_or_receiving: Literal["sending", "receiving"] = "sending",
         ligand: Optional[str] = None,
         receptor: Optional[str] = None,
         pathway: Optional[str] = None,
+        target: Optional[str] = None,
         sender_cell_type: Optional[str] = None,
         receiver_cell_type: Optional[str] = None,
         no_cell_type_markers: bool = False,
@@ -765,7 +766,6 @@ class MuSIC_Interpreter(MuSIC):
         or differential expression of genes in cells with high or low received signaling effect potential.
 
         Args:
-            target: Target to use for differential expression analysis. If None, will use the first listed target.
             diff_sending_or_receiving: Whether to compute differential expression of genes in cells with high ligand
                 expression or high or low "receiving effect" potential (defined as predicted influence of an
                 interaction event on downstream expression).
@@ -774,6 +774,8 @@ class MuSIC_Interpreter(MuSIC):
             receptor: Optional receptor to use for differential expression analysis. Needed if
                 'diff_sending_or_receiving' is 'receiving'. Will take precedent over sender/receiver cell type if
                 also provided.
+            target: Only used if 'diff_sending_or_receiving' is "receiving"- target to use for differential expression
+                analysis. If None, will use the first listed target.
             pathway: Optional pathway to use for differential expression analysis. Will use ligands and receptors in
                 these pathways to collectively compute signaling potential score. Will take precedent over
                 ligand/receptor and sender/receiver cell type if provided.
@@ -812,10 +814,11 @@ class MuSIC_Interpreter(MuSIC):
                         f"incorporated in the initial model."
                     )
 
-                if scipy.sparse.issparse(self.adata.X):
-                    self.adata.obs[f"{pathway}_ligands"] = self.adata[:, all_senders].X.sum(axis=1)
-                else:
-                    self.adata.obs[f"{pathway}_ligands"] = np.sum(self.adata[:, all_senders].X, axis=1)
+                self.adata.obs[f"{pathway}_ligands"] = np.sum(self.ligands_expr[all_senders].values, axis=1)
+                # if scipy.sparse.issparse(self.adata.X):
+                #     #self.adata.obs[f"{pathway}_ligands"] = self.adata[:, all_senders].X.sum(axis=1)
+                # else:
+                #     #self.adata.obs[f"{pathway}_ligands"] = np.sum(self.adata[:, all_senders].X, axis=1)
                 send_key = f"{pathway}_ligands"
 
             # For receiving analysis, retrieve (or compute) received effect potential and use as the independent
@@ -944,7 +947,7 @@ class MuSIC_Interpreter(MuSIC):
         if diff_sending_or_receiving == "sending":
             try:
                 # Sent signal from ligand
-                sent_signal = self.adata[:, send_key].X
+                sent_signal = self.ligands_expr[send_key]
             except:
                 try:
                     # Sent signal from pathway
@@ -979,10 +982,10 @@ class MuSIC_Interpreter(MuSIC):
             all_TFs = [tf for tf in all_TFs if tf in self.adata.var_names]
             all_RBPs = [r for r in all_RBPs if r in self.adata.var_names]
 
-            # Further subset list of TFs to those that are implicated in signaling patterns of interest and also
-            # are expressed in at least n% of the cells that are nonzero for sending potential (use the user input
-            # 'target_expr_threshold'):
-            nz_sending = list(self.sample_names[sent_signal != 0])
+            # Further subset list of TFs to those that are expressed in at least n% of the cells that are nonzero
+            # for sending potential (use the user input 'target_expr_threshold'):
+            indices = sent_signal.toarray().reshape(-1) != 0
+            nz_sending = list(self.sample_names[indices])
             adata_subset = self.adata[nz_sending, :]
             n_cells_threshold = int(self.target_expr_threshold * adata_subset.n_obs)
 
@@ -1005,7 +1008,7 @@ class MuSIC_Interpreter(MuSIC):
             high_variance_genes_filter = list(self.adata.var.index[gene_counts_stats.high_var.values])
             adata_hvg = self.adata[:, high_variance_genes_filter]
 
-            nz_receiving = np.any(received_signal != 0, axis=1)
+            nz_receiving = np.any(received_signal.toarray() != 0, axis=1)
             adata_subset = adata_hvg[nz_receiving, :]
             n_cells_threshold = int(self.target_expr_threshold * adata_subset.n_obs)
 
@@ -1021,7 +1024,8 @@ class MuSIC_Interpreter(MuSIC):
 
             if no_cell_type_markers:
                 # Remove cell type markers (more likely to be reflective than responsive to signaling) using a series of
-                # binomial models- each column is the p-value for a particular cell type group compared to all other groups
+                # binomial models- each column is the p-value for a particular cell type group compared to all other
+                # groups
                 p_values = pd.DataFrame(
                     index=adata_hvg.var_names,
                     columns=[
@@ -1080,6 +1084,29 @@ class MuSIC_Interpreter(MuSIC):
         # Compute q-values for each gene:
         GAM_adata, bs = DE_GAM_test(GAM_adata, bs)
         return GAM_adata, bs
+
+    def calc_sender_degs_all_ligands(self):
+        """For all ligands in the AnnData object, identify predicted upstream regulatory factors."""
+        if self.no_cell_type_markers is not None:
+            no_cell_type_markers = self.no_cell_type_markers
+        else:
+            no_cell_type_markers = False
+
+        # Directory to save differential testing results:
+        parent_dir = os.path.dirname(self.output_path)
+        sig_de_path = os.path.join(parent_dir, "signaling_effect_degs")
+
+        ligands = self.ligands_expr.columns
+
+        for ligand in ligands:
+            GAM_adata, bs = self.sender_receiver_effect_deg_detection(
+                diff_sending_or_receiving="sending",
+                ligand=ligand,
+                no_cell_type_markers=no_cell_type_markers,
+            )
+            GAM_adata.write_h5ad(os.path.join(sig_de_path, f"{ligand}.h5ad"))
+            with open(os.path.join(sig_de_path, f"{ligand}_bs.pkl"), "wb") as f:
+                pickle.dump(bs, f)
 
     def group_sender_receiver_effect_degs(
         self,
@@ -1224,10 +1251,6 @@ class MuSIC_Interpreter(MuSIC):
                 the genes that are most positively and most negatively enriched in relation to signaling effect
                 potential
         """
-
-        # Directory to save differential testing results:
-        parent_dir = os.path.dirname(self.output_path)
-        sig_de_path = os.path.join(parent_dir, "signaling_effect_degs")
 
         # Input checks- if not manually provided, check argparse inputs (make sure an input was given from somewhere,
         # though):

spateo/tools/ST_modeling/regression_utils.py
@@ -579,6 +579,11 @@ def fit_DE_GAM(
     if endog.ndim == 1:
         endog = endog.reshape(-1, 1)
 
+    if scipy.sparse.issparse(exog):
+        exog = exog.toarray()
+    if scipy.sparse.issparse(endog):
+        endog = endog.toarray()
+
     if diff_sending_or_receiving == "sending" and endog.shape[1] > 1:
         raise ValueError(
             "For this configuration, ('diff_sending_or_receiving' = 'sending'), dependent variable array must be 1D. "
@@ -593,6 +598,7 @@ def fit_DE_GAM(
     targets = {}
     predictors = {}
     subsetted_cells = {}
+    n_subset = {}
     # Measure converged and/or genes that had enough cells post-filtering to model:
     fitted = {}
     pvals = {}
@@ -603,107 +609,85 @@ def fit_DE_GAM(
     # misleading- we can use cells for which both signals are present to get a sense of the true context-independent
     # effect of one on the other:
 
-    # Check non-negativity:
-    if scipy.sparse.issparse(endog):
-        if not (endog.data >= 0).all():
-            raise ValueError("All values of the count matrix should be non-negative")
-    else:
-        if (endog < 0).any():
-            raise ValueError("All values of the count matrix should be non-negative")
-
-    if scipy.sparse.issparse(exog):
-        if not (exog.data >= 0).all():
-            raise ValueError("All values of the count matrix should be non-negative")
-    else:
+    if diff_sending_or_receiving == "receiving":
+        # Check non-negativity:
         if (exog < 0).any():
             raise ValueError("All values of the count matrix should be non-negative")
 
-    # In the case that differential expression is being done in the receiving cell, so there are multiple different
-    # targets:
-    if diff_sending_or_receiving == "receiving":
         for idx in range(endog.shape[1]):
             gene = genes[idx]
-            if scipy.sparse.issparse(endog):
-                # Independent variables can be multi-column in this case to use many pathway components
-                to_subset = np.nonzero(np.logical_and(np.any(exog != 0, axis=0), endog[:, idx].data != 0))[0]
-                # If the size of the subset is not large enough, set this gene to insignificant and set
-                # p-values and Wald stats to 1 and 0, respectively:
-                if len(to_subset) < 20:
-                    fitted[gene] = False
-                    pvals[gene] = 1
-                    wald_stats[gene] = 0
-                    continue
-                else:
-                    # Placeholders:
-                    fitted[gene] = True
-                    pvals[gene] = np.nan
-                    wald_stats[gene] = np.nan
+            to_subset = np.nonzero(np.logical_and(np.any(exog != 0, axis=1), endog[:, idx] != 0))[0]
+            # Outliers will obscure the true effect- remove them:
+            temp_endog = endog[to_subset, idx]
+            temp_exog = exog[to_subset, :]
+            z_scores_endog = (temp_endog - np.mean(temp_endog)) / np.std(temp_endog)
+            outliers_endog = np.where(np.abs(z_scores_endog) > 3)[0]
+            z_scores_exog = (temp_exog - np.mean(temp_exog)) / np.std(temp_exog)
+            outliers_exog = np.where(np.abs(z_scores_exog) > 3)[0]
+            outliers = np.union1d(outliers_endog, outliers_exog)
 
-                targets[gene] = endog[to_subset, idx].data
+            to_subset = np.setdiff1d(to_subset, outliers)
+
+            # If the size of the subset is not large enough, set this gene to insignificant and set p-values and Wald
+            # stats to 1 and 0, respectively:
+            if len(to_subset) < 20:
+                fitted[gene] = False
+                pvals[gene] = 1
+                wald_stats[gene] = 0
+                continue
             else:
-                to_subset = np.nonzero(np.logical_and(np.any(exog != 0, axis=0), endog[:, idx] != 0))[0]
-                # If the size of the subset is not large enough, set this gene to insignificant:
-                if len(to_subset) < 20:
-                    fitted[gene] = False
-                    pvals[gene] = 1
-                    wald_stats[gene] = 0
-                    continue
-                else:
-                    # Placeholders:
-                    fitted[gene] = True
-                    pvals[gene] = np.nan
-                    wald_stats[gene] = np.nan
-                targets[gene] = endog[to_subset, idx]
+                # Placeholders:
+                fitted[gene] = True
+                pvals[gene] = np.nan
+                wald_stats[gene] = np.nan
+            targets[gene] = endog[to_subset, idx]
 
             # The independent variable(s) are shared, but subsetting differences may exist, so store the determined
             # subset for each gene:
             predictors[gene] = exog[to_subset, :]
             # Store indices of subset:
             subsetted_cells[gene] = to_subset
+            n_subset[gene] = len(to_subset)
 
-    # In the case that differential expression is being done in the sending cell, so there are multiple different
-    # predictors for which models will be fit one-at-a-time:
     elif diff_sending_or_receiving == "sending":
+        # Check non-negativity:
+        if (endog < 0).any():
+            raise ValueError("All values of the count matrix should be non-negative")
+
         for idx in range(exog.shape[1]):
             gene = genes[idx]
-            if scipy.sparse.issparse(exog):
-                to_subset = np.nonzero(np.logical_and(endog != 0, exog[:, idx].data != 0))[0]
-                # If the size of the subset is not large enough, set this gene to insignificant and set
-                # p-values and Wald stats to 1 and 0, respectively:
-                if len(to_subset) < 20:
-                    fitted[gene] = False
-                    pvals[gene] = 1
-                    wald_stats[gene] = 0
-                    continue
-                else:
-                    # Placeholders:
-                    fitted[gene] = True
-                    pvals[gene] = np.nan
-                    wald_stats[gene] = np.nan
+            to_subset = np.nonzero(np.logical_and(np.any(endog != 0, axis=1), exog[:, idx] != 0))[0]
+            # Outliers will obscure the true effect- remove them:
+            temp_endog = endog[to_subset, :]
+            temp_exog = exog[to_subset, idx]
+            z_scores_endog = (temp_endog - np.mean(temp_endog)) / np.std(temp_endog)
+            outliers_endog = np.where(np.abs(z_scores_endog) > 3)[0]
+            z_scores_exog = (temp_exog - np.mean(temp_exog)) / np.std(temp_exog)
+            outliers_exog = np.where(np.abs(z_scores_exog) > 3)[0]
+            outliers = np.union1d(outliers_endog, outliers_exog)
 
-                predictors[gene] = exog[to_subset, idx].data
+            to_subset = np.setdiff1d(to_subset, outliers)
+
+            # If the size of the subset is not large enough, set this gene to insignificant and set p-values and Wald
+            # stats to 1 and 0, respectively:
+            if len(to_subset) < 20:
+                fitted[gene] = False
+                pvals[gene] = 1
+                wald_stats[gene] = 0
+                continue
             else:
-                to_subset = np.nonzero(np.logical_and(endog != 0, exog[:, idx] != 0))[0]
-                # If the size of the subset is not large enough, set this gene to insignificant and set
-                # p-values and Wald stats to 1 and 0, respectively:
-                if len(to_subset) < 20:
-                    fitted[gene] = False
-                    pvals[gene] = 1
-                    wald_stats[gene] = 0
-                    continue
-                else:
-                    # Placeholders:
-                    fitted[gene] = True
-                    pvals[gene] = np.nan
-                    wald_stats[gene] = np.nan
+                # Placeholders:
+                fitted[gene] = True
+                pvals[gene] = np.nan
+                wald_stats[gene] = np.nan
+            targets[gene] = exog[to_subset, idx]
 
-                predictors[gene] = exog[to_subset, idx]
-
-            # The dependent variable(s) are shared, but subsetting differences may exist, so store the determined
+            # The independent variable(s) are shared, but subsetting differences may exist, so store the determined
             # subset for each gene:
-            targets[gene] = endog[to_subset]
+            predictors[gene] = endog[to_subset, :]
             # Store indices of subset:
             subsetted_cells[gene] = to_subset
+            n_subset[gene] = len(to_subset)
 
     else:
         raise ValueError("The independent variable array must be 2D- one-to-one tests currently not supported.")
@@ -723,10 +707,9 @@ def fit_DE_GAM(
     # regression should be applied in this case. This will only happen in the case of a "sending" analysis where the
     # dependent variable is the cell type:
     binary_endog = False
-    if not scipy.sparse.issparse(endog):
-        if all(x in [0, 1] for x in endog):
-            binary_endog = True
-            family = sm.families.Binomial()
+    if all(x in [0, 1] for x in endog):
+        binary_endog = True
+        family = sm.families.Binomial()
 
     if not binary_endog:
         if family == "gaussian":
@@ -814,10 +797,19 @@ def fit_DE_GAM(
     sc_obj.var["pvals"] = pd.Series(pvals)
     sc_obj.var["wald_stats"] = pd.Series(wald_stats)
     sc_obj.var["subsetted_cells"] = pd.Series(subsetted_cells)
+    sc_obj.var["n_subset"] = pd.Series(n_subset)
     if label is not None:
         sc_obj.uns["label"] = label
 
     return sc_obj, basis
+
+
+class GAM_DummyModel:
+    """Dummy class to store whether model converged or not."""
+
+    def __init__(self, label: str, converged: bool = False):
+        self.label = label
+        self.converged = converged
 
 
 def counts_to_Gam(
@@ -859,6 +851,12 @@ def counts_to_Gam(
     # Fit without intercept
     formula_string = "y ~ -1 + " + " + ".join(colnames)
 
+    print(f"\n{label}")
+    print(f"\n{data.shape} for {label}")
+    from scipy.stats import pearsonr
+
+    r, _ = pearsonr(data["y"], data["x0"])
+    print(f"\nCorrelation between y and x0 for {label}: {r}")
     try:
         with warnings.catch_warnings():
             warnings.filterwarnings("error", category=ConvergenceWarning)
@@ -867,13 +865,13 @@ def counts_to_Gam(
             )
             gam_fit = gam_model.fit()
 
+            # Keep track of the label and whether the model converged:
+            gam_fit.label = label
+            gam_fit.converged = True
     except Exception:
         print(f"\nConvergence not achieved for {label}.")
-        converged = False
-
-    # Keep track of the label and whether the model converged:
-    gam_fit.label = label
-    gam_fit.converged = converged
+        # Dummy model to store results:
+        gam_fit = GAM_DummyModel(label, False)
 
     return gam_fit
 