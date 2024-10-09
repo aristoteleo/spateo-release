3b84fbc8cbf1e322f71e7deb41df5f04a0679a67
Starlitnightly <starlitnightly@163.com>
2024-10-10 04:05:18 +0800

    Added `CAST` and `STAGATE` to cluster

--- spateo/tools/cluster/__init__.py
+++ spateo/tools/cluster/__init__.py
@@ -1,5 +1,6 @@
+from ._stagate import pySTAGATE
 from .cluster_spagcn import spagcn_vanilla
-from .find_clusters import scc, smooth, spagcn_pyg
+from .find_clusters import CAST, kmeans_clustering, mclust_py, scc, smooth, spagcn_pyg
 from .utils import (
     compute_pca_components,
     ecp_silhouette,
