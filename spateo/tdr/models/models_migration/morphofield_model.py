fbf6a8ffda5b8d02462bdb4ca0a391d93c3a1700
YifanLu2000 <lyf048@whu.edu.cn>
2025-05-28 13:43:20 -0700

    Add construct_field_plain function for 3D vector field visualization

--- spateo/tdr/models/models_migration/morphofield_model.py
+++ spateo/tdr/models/models_migration/morphofield_model.py
@@ -9,8 +9,10 @@ try:
 except ImportError:
     from typing_extensions import Literal
 
+from ....logging import logger_manager as lm
 from ..utilities import add_model_labels, merge_models
 from .arrow_model import construct_arrows
+from .line_model import construct_lines
 
 
 def construct_field(
@@ -138,3 +140,118 @@ def construct_field_streams(
 
     streams_model = merge_models([streamlines, arrows])
     return streams_model, src, plot_cmap
+
+
+def construct_field_plain(
+    model: PolyData,
+    vf_key: str = "VecFld_morpho",
+    n_sampling: Optional[Union[int, np.ndarray]] = None,
+    sampling_method: str = "trn",
+    factor: float = 1.0,
+    key_added: str = "v_arrows",
+    label: Union[str, list, np.ndarray] = "vector field",
+    color: Union[str, list, dict, np.ndarray] = "gainsboro",
+    alpha: float = 1.0,
+    tip_factor: Union[int, float] = 5,
+    tip_radius: float = 0.2,
+    **kwargs,
+) -> Tuple[PolyData, Optional[str]]:
+    """
+    Create a 3D vector field arrows model with plain lines.
+
+    Args:
+        model: A model that provides coordinate information and vector information for constructing vector field models.
+        vf_key: The key under which are the active vector field information.
+        n_sampling: n_sampling is the number of coordinates to keep after sampling. If there are too many coordinates
+                    in start_points, the generated arrows model will be too complex and unsightly, so sampling is
+                    used to reduce the number of coordinates.
+        sampling_method: The method to sample data points, can be one of ``['trn', 'kmeans', 'random']``.
+        factor: Scale factor applied to scaling array.
+        key_added: The key under which to add the labels.
+        label: The label of vector field model.
+        color: Color to use for plotting model.
+        alpha: The opacity of the color to use for plotting model.
+        tip_factor: Scale factor applied to scaling the tips.
+        tip_radius: Radius of the tips.
+        **kwargs: Additional parameters that will be passed to ``construct_arrows`` function.
+
+    Returns:
+        lines_model: 3D vector field model with arrows and lines.
+        plot_cmap: Recommended colormap parameter values for plotting.
+    """
+
+    from dynamo.tools.sampling import sample
+
+    start_points = np.asarray(model.points)
+    direction = np.asarray(model[vf_key]) * factor
+
+    index_arr = np.arange(0, start_points.shape[0])
+
+    if isinstance(label, (str, int, float)) or label is None:
+        labels = ["vector field"] * len(index_arr) if label is None else [label] * len(index_arr)
+    elif isinstance(label, (list, np.ndarray)) and len(label) == len(index_arr):
+        labels = label
+    else:
+        raise ValueError("`label` value is wrong.")
+
+    if (n_sampling is not None) and (isinstance(n_sampling, int)):
+        index_arr = sample(
+            arr=index_arr,
+            n=n_sampling,
+            method=sampling_method,
+            X=start_points,
+        )
+    elif (n_sampling is not None) and (isinstance(n_sampling, np.ndarray)):
+        index_arr = n_sampling
+    else:
+        if index_arr.shape[0] > 500:
+            lm.main_warning(
+                f"The number of cells is more than 500. You may want to " f"lower the max number of arrows to draw."
+            )
+
+    se_ind = 0
+    trajectories_points, trajectories_edges, trajectories_labels = [], [], []
+    tips_points, tips_vectors, tips_labels = [], [], []
+    for ind in index_arr:
+        trajectory_points = np.concatenate([start_points[[ind]], start_points[[ind]] + direction[[ind]]], axis=0)
+        n_states = len(trajectory_points)
+        trajectory_edges = np.concatenate(
+            [
+                np.arange(se_ind, se_ind + n_states - 1).reshape(-1, 1),
+                np.arange(se_ind + 1, se_ind + n_states).reshape(-1, 1),
+            ],
+            axis=1,
+        )
+        se_ind += n_states
+        trajectories_points.append(trajectory_points)
+        trajectories_edges.append(trajectory_edges)
+        trajectories_labels.extend([labels[ind]] * n_states)
+
+        tips_points.append(trajectory_points[-1])
+        tips_vectors.append(trajectory_points[-1] - trajectory_points[-2])
+        tips_labels.append(labels[ind])
+
+    streamlines, plot_cmap = construct_lines(
+        points=np.concatenate(trajectories_points, axis=0),
+        edges=np.concatenate(trajectories_edges, axis=0),
+        key_added=key_added,
+        label=np.asarray(trajectories_labels),
+        color=color,
+        alpha=alpha,
+    )
+
+    arrows, plot_cmap = construct_arrows(
+        start_points=np.asarray(tips_points),
+        direction=np.asarray(tips_vectors),
+        arrows_scale=np.ones(shape=(len(tips_points), 1)),
+        factor=tip_factor,
+        tip_length=1,
+        tip_radius=tip_radius,
+        key_added=key_added,
+        label=np.asarray(tips_labels),
+        color=color,
+        alpha=alpha,
+        **kwargs,
+    )
+    lines_model = merge_models([streamlines, arrows])
+    return lines_model, plot_cmap
